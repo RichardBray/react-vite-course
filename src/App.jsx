1991d1065d2fffa45b42f41b3bba64fcc8066ce9
Richard Oliver Bray <richardbray89@gmail.com>
2023-01-10 10:17:30 +0000

    chore: inital commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Card from "./components/Card";
+
+function App() {
+  return <Card />
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Description</p>
+        </div>
+    )
+}
+
+export default Card;
