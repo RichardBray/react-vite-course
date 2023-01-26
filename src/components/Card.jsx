5d631e18b6dc588cdef1bf53f73cce3c7189ab5c
Richard Oliver Bray <richardbray89@gmail.com>
2023-01-26 09:40:31 +0000

    chore: fix merge conflict

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
-function Card() {
-    return (
-        <div>
-            <h1>Title</h1>
-            <p>Description</p>
-=======
 function Card({title = "Default title", description= "Desc", children}) {
     return (
         <div>
             <h1>{title}</h1>
             <p>{description}</p>
             <div>{children}</div>
->>>>>>> Stashed changes
         </div>
     )
 }
