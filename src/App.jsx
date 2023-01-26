3b2a90418f8ecb7a96e0c1db0657c5e35531baa2
Richard Oliver Bray <richardbray89@gmail.com>
2023-01-26 09:35:26 +0000

    chore: add code for children talk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Card from "./components/Card";
 
 function App() {
+<<<<<<< Updated upstream
   return <Card />
+=======
+  return (
+    <Card title="Test">
+      <Card title="hello" description='world'/>
+    </Card>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 function Card() {
     return (
         <div>
             <h1>Title</h1>
             <p>Description</p>
+=======
+function Card({title = "Default title", description= "Desc", children}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <div>{children}</div>
+>>>>>>> Stashed changes
         </div>
     )
 }
