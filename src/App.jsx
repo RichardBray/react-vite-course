28e6bc6db376a2e2717e263baffabfe1c02de23d
Richard Oliver Bray <richardbray89@gmail.com>
2023-01-26 09:35:47 +0000

    fix: merge issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import './App.css'
 import Card from "./components/Card";
 
 function App() {
-<<<<<<< Updated upstream
-  return <Card />
-=======
   return (
     <Card title="Test">
       <Card title="hello" description='world'/>
     </Card>
   );
->>>>>>> Stashed changes
 }
 
 export default App
