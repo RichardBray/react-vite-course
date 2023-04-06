8fc8a788738a6b631a4a91e974444751ca686527
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-06 12:20:59 +0100

    chore: add ErrorBoundary component

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
