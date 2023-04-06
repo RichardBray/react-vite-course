5ac0dce726013c6afc1fc107a5c5af7d20a7ff3a
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-06 17:03:55 +0100

    chore: remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import { lazy, Suspense } from 'react';
 import './App.css';
 import Card from './components/Card';
-import ErrorBoundary from './components/ErrorBoundary'
-import { videos } from './video-data';
-
-const Details = lazy(() => {
-  return Promise.reject("fail");
-});
 
 function App() {
   return (
     <>
-      <ErrorBoundary fallback={<div>Something's gone wrong :(</div>}>
-        <Details />
-      </ErrorBoundary>
       {videos.map((video) => (
         <Card key={video.id} video={video} />
       ))}
