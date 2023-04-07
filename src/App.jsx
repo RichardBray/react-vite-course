06b4d45f63ec5b26de1e1673d31f6f2818a82a10
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-07 16:55:38 +0100

    style: add styles to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { lazy, Suspense } from 'react';
-import './App.css';
 import Card from './components/Card';
-
+import { videos } from './video-data.js';
 function App() {
+  const darkMode = true;
+  const styles = darkMode
+    ? { color: 'white', backgroundColor: 'black' }
+    : { color: 'black', backgroundColor: 'white' };
+
   return (
-    <>
+    <div style={{ ...styles, fontFamily: 'sans-serif', textAlign: 'center' }}>
       {videos.map((video) => (
         <Card key={video.id} video={video} />
       ))}
-    </>
+    </div>
   );
 }
 
