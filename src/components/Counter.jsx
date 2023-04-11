f3fe81a2ce903560620e209559c7dc1f75c222df
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-11 11:51:46 +0100

    style(Counter.jsx): add div to display count of clicks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-
   function handleClick() {
     setCount((prevCount) => prevCount + 1);
   }
 
   return (
     <>
+      <div>You clicked {count} times</div>
       <button onClick={handleClick}>Click me</button>
     </>
   );
