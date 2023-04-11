130cf9197c0abec59b26392476e43b82499a9859
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-11 14:50:11 +0100

    refactor(Counter.jsx): replace useState with useReducer to manage state of Counter component feat(Counter.jsx): add increment, decrement and reset functionality to Counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const initialState = { count: 0 };
 
+  function counterReducer(state, action) {
+    const actions = {
+      increment: () => ({ count: state.count + 1 }),
+      decrement: () => ({ count: state.count - 1 }),
+      reset: () => initialState,
+    };
 
-  function handleClick() {
-    setCount((prevCount) => prevCount + 1);
+    return actions[action.type] ? actions[action.type]() : state;
   }
+
+  const [state, dispatch] = useReducer(counterReducer, initialState);
 
   return (
     <>
-      <button onClick={handleClick}>Click me</button>
+      <div>Count value: {state.count}</div>
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 }
