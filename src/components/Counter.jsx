47a8b0f36eea329ad3d8344eec3dd8f4466804aa
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-11 11:50:21 +0100

    refactor(Counter.jsx): remove console logs and unused code feat(Counter.jsx): remove Count + 1 and Count + 2 divs and only display clicked count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    console.log('component has mounted')
-    setMounted(true)
-  }, []);
-
-  useEffect(() => {
-    if (mounted) {
-      console.log('count incremented')
-    }
-    return () => console.log('component has unmounted');
-  }, [count]);
 
   function handleClick() {
     setCount((prevCount) => prevCount + 1);
@@ -22,9 +10,6 @@ export default function Counter() {
 
   return (
     <>
-      <div>You clicked {count} times</div>
-      <div>Count + 1: {count + 1}</div>
-      <div>Count + 2: {count + 2}</div>
       <button onClick={handleClick}>Click me</button>
     </>
   );
